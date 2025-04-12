108cacd673fe19dd52f852431053bfbca38392a3
Wilinton <wilintongabrielss1@gmail.com>
2025-04-12 17:52:50 -0300

    feat: implementa repository da entidade

---
+++ src/main/java/com/url_shortener/repository/UrlMappingRepository.java
@@ -0,0 +1,9 @@
+package com.url_shortener.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.url_shortener.model.UrlMapping;
+
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long>{
+
+}
