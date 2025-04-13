318264ad63c2fb6eee1bbfdb020542bf9586afef
Wilinton <wilintongabrielss1@gmail.com>
2025-04-12 23:27:43 -0300

    feat: adiciona validação no UrlRequestDto

--- src/main/java/com/url_shortener/dto/url/UrlRequestDto.java
+++ src/main/java/com/url_shortener/dto/url/UrlRequestDto.java
@@ -1,8 +1,11 @@
 package com.url_shortener.dto.url;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UrlRequestDto {
+
+    @NotBlank
     private String urlLonga;
 }
